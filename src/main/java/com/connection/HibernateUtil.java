4b0d76458ab047f4547d3b8996659f6676187d67
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-03-16 10:32:01 +0100

    cambios

--- src/main/java/com/connection/HibernateUtil.java
+++ src/main/java/com/connection/HibernateUtil.java
@@ -17,15 +17,11 @@ public class HibernateUtil {
         	configuration.configure();
         	 
         	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-//        	System.out.println(configuration.getProperty("hibernate.connection.url"));
         	sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            // Create the SessionFactory from hibernate.cfg.xml
-//            return new Configuration().configure(new File("hibernate.cgf.xml")).buildSessionFactory();
 
         	return sessionFactory;
         }
         catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed. " + ex);
             throw new ExceptionInInitializerError(ex);
         }
@@ -36,7 +32,6 @@ public class HibernateUtil {
     }
   
     public static void shutdown() {
-        // Close caches and connection pools
         getSessionFactory().close();
     }
 }
