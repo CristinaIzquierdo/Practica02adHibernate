36c6a8126524446c81b362e62955948649715a44
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-03-16 10:32:44 +0100

    cambios

--- src/main/java/com/connection/Main.java
+++ src/main/java/com/connection/Main.java
@@ -8,28 +8,19 @@ import model.Cliente;
 
 public class Main {
 	public static void main (String[] args) throws SQLException {
-//		Conexion.conectar();
-//		System.exit(0);
-//		try {
+
 			Session session = HibernateUtil.getSessionFactory().openSession();
 			
 			session.beginTransaction();
 			Cliente c = new Cliente(666);
 			
-			c.setNombreCliente("daniel");
-			c.setNombreContacto("Daniel");
+			c.setNombreCliente("cristina");
+			c.setNombreContacto("cristina");
 			c.setCiudad("Zaragoza");
-			c.setTelefono("657464532");
-			c.setFax("989548874");
-			c.setLineaDireccion1("sadasd");
+			c.setTelefono("944865748");
+			c.setFax("9448657487");
+			c.setLineaDireccion1("casfdsfdsfdf");
 			session.save(c);
-			session.getTransaction().commit();
-//		} catch (ExceptionInInitializerError e) {
-//			System.out.println(" ====> ERROR: " + e.getMessage());
-//		}
-		 
-       
- 
-		
+			session.getTransaction().commit();		
 	}
 }
